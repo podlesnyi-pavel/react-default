b50c9a20de875caf31c99be1c4752502d07ad74b
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-12-03 17:57:38 +0200

    Updates

---
+++ static/js/main.0e9fafa1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-default"]=this["webpackJsonpreact-default"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(3),r=c.n(n),s=c(0),a=function(){return Object(s.jsx)("h2",{children:"template"})},u=function(){return Object(s.jsxs)("div",{className:"App",children:[Object(s.jsx)("h1",{children:"default"}),Object(s.jsx)(a,{})]})};r.a.render(Object(s.jsx)(u,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.0e9fafa1.chunk.js.map
